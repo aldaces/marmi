378f8628af43dd253e0bb6b28fd3aeeaa7d337eb
aldaces <shadarsi@gmail.com>
2021-01-24 21:31:29 +0200

    +pages

--- js/common.js
+++ js/common.js
@@ -97,3 +97,36 @@ $(document).ready(function ($) {
     })
 });
 
+
+$(document).ready(function ($) {
+    var $grid = $('.stone-product-list').isotope({
+        itemSelector: '.stone-product-item',
+        masonry: {
+            columnWidth: 100,
+            fitWidth: true
+        }
+      });
+      // filter functions
+      var filterFns = {
+        // show if name ends with -ium
+        ium: function() {
+          var name = $(this).find('.name').text();
+          return name.match( /ium$/ );
+        }
+      };
+      // bind filter button click
+      $('.filters-button-group').on( 'click', 'button', function() {
+        var filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+      });
+      // change is-checked class on buttons
+      $('.button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'button', function() {
+          $buttonGroup.find('.is-checked').removeClass('is-checked');
+          $( this ).addClass('is-checked');
+        });
+      });
+});
